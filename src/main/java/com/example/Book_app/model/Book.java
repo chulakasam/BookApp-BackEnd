4402d0de60f3cf54cce9ab17adcc2dff907f0c97
chulakasam <sampathchulaka33872@gmail.com>
2025-01-01 14:59:25 +0530

    book,googlebookresponse class done

--- src/main/java/com/example/Book_app/model/Book.java
+++ src/main/java/com/example/Book_app/model/Book.java
@@ -1,4 +1,14 @@
 package com.example.Book_app.model;
 
 public class Book {
+    private VolumeInfo volumeInfo;
+
+    public VolumeInfo getVolumeInfo() {
+        return volumeInfo;
+    }
+
+    public void setVolumeInfo(VolumeInfo volumeInfo) {
+        this.volumeInfo = volumeInfo;
+    }
+
 }
--- src/main/java/com/example/Book_app/model/GoogleBooksResponse.java
+++ src/main/java/com/example/Book_app/model/GoogleBooksResponse.java
@@ -1,4 +1,16 @@
 package com.example.Book_app.model;
 
+import java.util.List;
+
 public class GoogleBooksResponse {
+    private List<Book> items;
+
+    public List<Book> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Book> items) {
+        this.items = items;
+    }
+
 }
