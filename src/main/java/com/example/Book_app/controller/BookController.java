b76ab5a32a44df0351e298876757082581af024c
chulakasam <sampathchulaka33872@gmail.com>
2025-01-01 15:11:48 +0530

    dependancy issue fix and book controller done

--- src/main/java/com/example/Book_app/controller/BookController.java
+++ src/main/java/com/example/Book_app/controller/BookController.java
@@ -1,9 +1,26 @@
 package com.example.Book_app.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Book_app.model.Book;
+import com.example.Book_app.model.GoogleBooksResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:63342")
 public class BookController {
+    private final String GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes";
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestParam String query) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = GOOGLE_BOOKS_API_URL + "?q=" + query + "&maxResults=10";
+        ResponseEntity<GoogleBooksResponse> response = restTemplate.getForEntity(url, GoogleBooksResponse.class);
+
+        return response.getBody().getItems();
+    }
+
 }
