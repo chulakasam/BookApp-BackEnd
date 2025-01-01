09dea2f4d88b97fb975828755029bdb073a7024f
chulakasam <sampathchulaka33872@gmail.com>
2025-01-01 14:54:27 +0530

    project create

---
+++ src/main/java/com/example/Book_app/controller/BookController.java
@@ -0,0 +1,9 @@
+package com.example.Book_app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BookController {
+}
