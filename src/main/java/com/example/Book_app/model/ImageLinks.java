1ca8df358c2024edcc71d444fb4bd7486f4b44cd
chulakasam <sampathchulaka33872@gmail.com>
2025-01-01 15:02:23 +0530

    imagelink,volume info done

--- src/main/java/com/example/Book_app/model/ImageLinks.java
+++ src/main/java/com/example/Book_app/model/ImageLinks.java
@@ -1,4 +1,13 @@
 package com.example.Book_app.model;
 
 public class ImageLinks {
+    private String thumbnail;
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
 }
